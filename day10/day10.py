0795247c9a9e5e3fa9dba40ccf9ba6c0a3e269c4
Joachim Strömbergson <joachim@assured.se>
2022-12-13 13:56:46 +0100

    Add initial input and solution to problem one for AoC 2015 day10
    Signed-off-by: Joachim Strömbergson <joachim@assured.se>

--- day10/day10.py
+++ day10/day10.py
@@ -12,46 +12,71 @@
 
 # Imports as needed.
 
-
 #-------------------------------------------------------------------
-# CPU is model of the constrained cpu.
+# CPU is a model of the constrained cpu.
 #-------------------------------------------------------------------
 class CPU:
-    def __init__(self, program):
-        self.state = 'INIT'
+    def __init__(self, filename):
+        self.state = 'START'
         self.x = 1
-        self.cycles = 1
-        self.icycles = 0
+        self.dx = 0
+        self.cycle = 1
+        self.instr = ""
         self.pc = 0
-        self.program = program
+        self.finished = False
+
+        self.program = []
+        with open(filename,'r') as f:
+            for line in f:
+                self.program.append(line.strip())
 
 
-#-------------------------------------------------------------------
-#-------------------------------------------------------------------
-def get_input(filename):
-    l = []
-    with open(filename,'r') as f:
-        for line in f:
-            l.append(line.strip())
-    return l
+    def update(self):
+        if self.state == 'START':
+            if self.pc == len(self.program):
+                self.finished = True
+            else:
+                self.instr = self.program[self.pc]
+                self.pc += 1
+
+#                print("Cycle", self.cycle, "Starting instruction", self.instr, " x =", self.x)
+
+                if 'noop' in self.instr:
+                    self.dx = 0
+                    self.state = 'COMPLETE'
+
+                else:
+                    self.dx = int(self.instr.split(' ')[1])
+                    self.state = 'EXECUTE'
 
 
-#-------------------------------------------------------------------
-#-------------------------------------------------------------------
-def cpu_get_strength(cpu):
-    return cpu.cycles * cpu.x
+        elif self.state == 'EXECUTE':
+#            print("Cycle", self.cycle, "Executing instruction", self.instr, " x =", self.x)
+            self.state = 'COMPLETE'
+            self.cycle += 1
 
 
-#-------------------------------------------------------------------
-#-------------------------------------------------------------------
-def cpu_step(cpu):
-    pass
+        elif self.state == 'COMPLETE':
+#            print("Cycle", self.cycle, "Completing instruction", self.instr, " x =", self.x)
+            self.state = 'START'
+            self.x += self.dx
+            self.cycle += 1
 
 
-#-------------------------------------------------------------------
-#-------------------------------------------------------------------
-def cpu_done(cpu):
-    return cpu.state == 'DONE'
+    def get_cycle(self):
+        return self.cycle
+
+
+    def get_x(self):
+        return self.x
+
+
+    def get_strength(self):
+        return self.cycle * self.x
+
+
+    def done(self):
+        return self.finished
 
 
 #-------------------------------------------------------------------
@@ -60,9 +85,23 @@ def problem1():
     print("Problem 1")
     print("---------")
 
-    my_program = get_input("day10_input.txt")
-    my_program = get_input("day10_short_example.txt")
-    print(my_program)
+#    my_cpu = CPU("day10_short_example.txt")
+#    my_cpu = CPU("day10_long_example.txt")
+    my_cpu = CPU("day10_input.txt")
+
+    total_strength = 0
+    current_strength = 0
+    cycle_list = [20, 60, 100, 140, 180, 220]
+
+    while not my_cpu.done():
+        if my_cpu.get_cycle() in cycle_list:
+            current_strength = my_cpu.get_strength()
+            total_strength += current_strength
+#            print("cycle", my_cpu.get_cycle(), "current strength", current_strength, "total_strength", total_strength)
+            cycle_list = cycle_list[1:]
+        my_cpu.update()
+
+    print("Total strength:", total_strength)
     print("")
 
 
@@ -71,10 +110,6 @@ def problem1():
 def problem2():
     print("Problem 2")
     print("---------")
-
-    my_program = get_input("day10_input.txt")
-    my_program = get_input("day10_short_example.txt")
-    print(my_program)
     print("")
 
 
