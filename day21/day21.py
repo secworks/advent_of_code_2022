bb57c19aa48430b236b06aa940e2c52846009beb
Joachim Strömbergson <joachim@assured.se>
2022-12-22 10:22:27 +0100

    Add solution for AoC 2022, day21, problem one
    Signed-off-by: Joachim Strömbergson <joachim@assured.se>

--- day21/day21.py
+++ day21/day21.py
@@ -25,14 +25,59 @@ def get_input(filename):
 
 #-------------------------------------------------------------------
 #-------------------------------------------------------------------
+def get_monkey_db(l):
+    monkey_db = {}
+
+    for n in l:
+        if len(n.split(' ')) == 2:
+            (name, value) = n.split(' ')
+            monkey_db[name[:-1]] = ('resolved', value, None, None, None)
+        else:
+            (name, lname, op, rname) = n.split(' ')
+            monkey_db[name[:-1]] = ('unresolved', None, lname, op, rname)
+    return monkey_db
+
+
+#-------------------------------------------------------------------
+# Given a monkey name and a node db, resolve the yell value.
+#-------------------------------------------------------------------
+def resolve_yell(name, db):
+    (status, value, lname, op, rname) = db[name]
+
+    if status == 'resolved':
+        return int(value)
+
+    else:
+        lvalue = resolve_yell(lname, db)
+        rvalue = resolve_yell(rname, db)
+
+        if op == '+':
+            return lvalue + rvalue
+
+        elif op == '-':
+            return lvalue - rvalue
+
+        elif op == '*':
+            return lvalue * rvalue
+
+        elif op == '/':
+            return int(lvalue / rvalue)
+
+
+#-------------------------------------------------------------------
+#-------------------------------------------------------------------
 def problem1():
     print("Problem 1")
     print("---------")
 
     my_input = get_input("day21_input.txt")
-    my_input = get_input("day21_example.txt")
+#    my_input = get_input("day21_example.txt")
 
-    print(my_input)
+    my_monkey_db = get_monkey_db(my_input)
+    print(my_monkey_db)
+    my_yell = resolve_yell('root', my_monkey_db)
+
+    print("root will yell", my_yell)
     print("")
 
 
